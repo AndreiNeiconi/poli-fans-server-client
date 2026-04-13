ae338158b5cd58c47a6cbdc9bc6561809f054825
Neiconi Andrei <a.neiconi19@gmail.com>
2026-04-13 16:45:40 +0300

    feat: add class-transformer and class-validator dependencies; update CreateUserDto with validation decorators

--- src/users/dto/create-usere.dto.ts
+++ src/users/dto/create-usere.dto.ts
@@ -1,7 +1,18 @@
+import { IsEmail, IsOptional, IsString ,MinLength, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   first_name?: string;
+  @IsString()
+  @IsNotEmpty()
   last_name?: string;
+  @IsString()
+  @IsNotEmpty()
   username?: string;
+  @IsEmail()
+  @IsNotEmpty()
   email?: string;
+  @IsString()
+  @MinLength(8)
   password?: string; // Plain text from the request, we hash it in the service
 }
