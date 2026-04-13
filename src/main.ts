6aed4b93be28ea8572a0411f172253bb133c6b2b
Neiconi Andrei <a.neiconi19@gmail.com>
2026-04-13 18:07:29 +0300

    test for bug fix porpouse

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+console.log('Environment variables loaded:', {
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  PORT: process.env.PORT,
+});
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
