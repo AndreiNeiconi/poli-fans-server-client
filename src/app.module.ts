2a306962108754558552eeac761c23dbec9d6bd0
Neiconi Andrei <a.neiconi19@gmail.com>
2026-04-13 17:22:24 +0300

    feat: update @nestjs/config to 4.0.4 and add @nestjs/typeorm dependency; configure TypeORM in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { ProfilesModule } from './profiles/profiles.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Type } from 'class-transformer';
 
 @Module({
-  imports: [ProfilesModule, UsersModule,DatabaseModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port : process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // Set to false in production
+    })
+    
+    ,ProfilesModule, UsersModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
