f45bb2644e9b4c090416dac6294e50ec4bbd3002
Neiconi Andrei <a.neiconi19@gmail.com>
2026-04-23 22:24:42 +0300

    Bugfix: Remove file manager module and related files

--- src/file-manager/file-manager.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { FileManagerService } from './file-manager.service';
-
-@Controller('file-manager')
-export class FileManagerController {
-  constructor(private readonly fileManagerService: FileManagerService) {}
-  
-
-}
--- src/file-manager/file-manager.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FileManagerService } from './file-manager.service';
-import { FileManagerController } from './file-manager.controller';
-
-@Module({
-  controllers: [FileManagerController],
-  providers: [FileManagerService],
-})
-export class FileManagerModule {}
