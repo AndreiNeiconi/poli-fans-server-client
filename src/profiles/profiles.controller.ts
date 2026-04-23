0bb34e825a2481285ece5a6bd1e60a1536faafa3
Neiconi Andrei <a.neiconi19@gmail.com>
2026-04-24 00:02:30 +0300

    feat: implement ProfilesController and ProfilesService with authentication guard

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Get, Param, Post, Put, Query,Delete,HttpCode,HttpStatus } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query,Delete,HttpCode,HttpStatus, UseGuards, Req } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfilesDto } from './dto/update-profile.dto';
+import { ProfilesService } from './profiles.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('profiles')
 export class ProfilesController {
+    constructor(private readonly profilesService: ProfilesService,private AuthGurd:AuthGuard) { }
+    @Get()
+    @UseGuards(AuthGuard)
+    async getProfile(@Req() req: any) {
+        
+        const userId = req.user.id;
+        return this.profilesService.getProfile(userId);
+    }
 
 
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PG_CONNECTION } from '../database/database.module';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+    constructor(@Inject(PG_CONNECTION) private conn: any) { }
+    
+    async getProfile(userId: number) {
+        const query = 'SELECT * FROM user_display_profiles WHERE id = $1';
+        const res = await this.conn.query(query, [userId]);
+        return res.rows[0];
+    }
+}
