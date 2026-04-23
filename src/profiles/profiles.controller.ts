8c366d20b6ec29f89756fdeb4986d156581b0f83
Neiconi Andrei <a.neiconi19@gmail.com>
2026-04-23 22:19:50 +0300

    refactor: remove unused profile controller methods

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -4,31 +4,6 @@ import { UpdateProfilesDto } from './dto/update-profile.dto';
 
 @Controller('profiles')
 export class ProfilesController {
-    @Get()
-    findeAll(@Query('age') age: number) {
-        return [{ age }];
-    }
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return { id };
-    }
-    @Post()
-    create(@Body() createProfileDto: CreateProfileDto) {
-        return {
-            name: createProfileDto.name,
-            description: createProfileDto.description
-};
-    }
-    @Put(':id')
-    update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfilesDto) {
-        return {
-            id,
-            name: updateProfileDto.name,
-            description: updateProfileDto.description
-        };
-    }
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    remove(@Param('id') id: string) { }
+
 
 }
