2ee68a81ca8d338ea46e3e5d7440324151a88cb4
Neiconi Andrei <a.neiconi19@gmail.com>
2026-05-11 17:28:21 +0300

    compleate the services

--- src/filemanager/filemanager.service.ts
+++ src/filemanager/filemanager.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateFilemanagerDto } from './dto/create-filemanager.dto';
 import { UpdateFilemanagerDto } from './dto/update-filemanager.dto';
+import { PG_CONNECTION } from '../database/database.module';
 
 @Injectable()
 export class FilemanagerService {
-  create(createFilemanagerDto: CreateFilemanagerDto) {
-    return 'This action adds a new filemanager';
+  constructor(@Inject(PG_CONNECTION) private conn: any){}
+  async create(file: Express.Multer.File) {
+    const qurry = `
+    INSERT INTO media_files (original_name, internal_name, file_path, mime_type, size_bytes, uploaded_by)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+    `
+    const values = [
+    file.originalname,
+    file.filename,
+    file.destination,
+    file.mimetype,
+    file.size,
+    null // Momentan punem null pentru uploaded_by până legăm Auth-ul
+  ];
+    const res = await this.conn.query(qurry, values)
+    
+    return res.rows[0];
   }
 
   findAll() {
