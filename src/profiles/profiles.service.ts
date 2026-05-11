915e3d3ce3d5e0fab6eb0e8772c408de2c2b4ba8
Neiconi Andrei <a.neiconi19@gmail.com>
2026-05-11 13:00:02 +0300

    bug fix

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -6,7 +6,7 @@ export class ProfilesService {
     constructor(@Inject(PG_CONNECTION) private conn: any) { }
     
     async getProfile(userId: number) {
-        const query = 'SELECT * FROM user_display_profiles WHERE id = $1';
+        const query = 'SELECT * FROM user_profiles WHERE id = $1';
         const res = await this.conn.query(query, [userId]);
         return res.rows[0];
     }
