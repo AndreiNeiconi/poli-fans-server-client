ec6c33fc4852db99cb4edfabb320ae3509890e8a
Andrei Neiconi <a.neiconi19@gmail.com>
2026-09-19 12:42:17 +0300

    fix: update get_imag method to use route parameter and improve error logging in ImagUrlSystemService

--- src/imag-url-system/imag-url-system.controller.ts
+++ src/imag-url-system/imag-url-system.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 import { ImagUrlSystemService } from './imag-url-system.service';
 
 @Controller('imag-url-system')
 export class ImagUrlSystemController {
   constructor(private readonly imagUrlSystemService: ImagUrlSystemService) {}
-  @Get()
-  async get_imag(@Req() req:any){
-    const mediaId =req
+  @Get(':id')
+  async get_imag(@Param('id') id:string){
+    const mediaId =id
 
     return this.imagUrlSystemService.getImg(mediaId)
   }
--- src/imag-url-system/imag-url-system.service.ts
+++ src/imag-url-system/imag-url-system.service.ts
@@ -1,15 +1,21 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { PG_CONNECTION } from '../database/database.module';
 
 @Injectable()
 export class ImagUrlSystemService {
     constructor(@Inject(PG_CONNECTION) private conn: any){}
 
-    async getImg(mediaId: number) {
+    async getImg(mediaId: string) {
         const query = 'SELECT file_path FROM media_files WHERE id = $1';
         const res = await this.conn.query(query, [mediaId]);
         if (!res.rows[0]) { 
-            console.log("nothing found")
+            console.log("No image was found")
+            throw new HttpException(
+                'Not Found',
+                HttpStatus.NOT_FOUND,
+                
+            );
+            
         }
         return res.rows[0];
     }
