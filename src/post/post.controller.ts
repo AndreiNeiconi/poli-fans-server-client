3c7ae2fd413cfd294a8e312755bf01efb81ec0b5
Andrei Neiconi <a.neiconi19@gmail.com>
2026-09-21 00:57:16 +0300

    feat: add missing return statement in create_text_only_post method

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { AuthGuard } from '../auth/auth.guard';
 import { CreatePostDto } from './dto/create-post.dto';
+import { time } from 'console';
 
 @Controller('post')
 export class PostController {
@@ -11,5 +12,6 @@ export class PostController {
   async create_text_only_post(@Req() req:any,@Body() post:CreatePostDto)
   {
     const  userId = req.user.sub;
+    return this.postService.create_post(post,userId)
   }
 }
