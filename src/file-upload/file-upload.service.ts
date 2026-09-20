b007d58a0af9691b5dfec1fa1cc484311ab35be6
Andrei Neiconi <a.neiconi19@gmail.com>
2026-09-20 14:33:59 +0300

    fix: update file path construction in handleFileUpload method to use file.path

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -15,7 +15,7 @@ export class FileUploadService {
         const values = [
             file.originalname,
             file.filename,
-            `${file.destination}/${file.fieldname}`,
+            file.path,
             file.mimetype,
             file.size,
             userID,
