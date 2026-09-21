65c1e1f66c4023bd8bd96804c953b06b983ce659
Andrei Neiconi <a.neiconi19@gmail.com>
2026-09-22 02:39:32 +0300

    feat: fix SQL query in create_post method and enhance error handling

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,5 +1,5 @@
 import { UUID } from 'crypto';
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { inspect } from 'util';
 import { PG_CONNECTION } from '../database/database.module';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -8,15 +8,26 @@ import { CreatePostDto } from './dto/create-post.dto';
 export class PostService {
     constructor(@Inject(PG_CONNECTION) private conn:any){}
     async create_post(post:CreatePostDto,id:string){
-        const query = `INSERT INTO posts (create_at,title,content,id_user_post) VALUES (NOW(), $1, $2, $3 );
-    RETURNING id`;
+        const query = `INSERT INTO posts (create_at,title,content,id_user_post) VALUES (NOW(), $1, $2, $3 )
+    RETURNING id_post`;
     const values = [
             post.title,
             post.content,
             id
             
         ];
-        const res = this.conn.query(query)
+        const res = await this.conn.query(query,values)
+        if(!res.rows[0]){
+        
+            throw new InternalServerErrorException('Post could not be created');
+
+        }
+        return {
+            id_post: res.rows[0].id_post,
+            message: 'Post created successfully',
+};
+        
         
     }
+
 }
